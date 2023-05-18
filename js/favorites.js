16020537440edbc99cfb9f57e91db92bb0474668
Lucas Fagundes <lucas_fagundesfranco@hotmail.com>
2023-05-18 13:23:36 -0300

    GitFavorites 2.0

--- js/favorites.js
+++ js/favorites.js
@@ -81,7 +81,7 @@ export class FavoritesView extends Favorites {
             row.querySelector('.user img').alt = `Imagem de ${user.name}`
             row.querySelector('.user p').textContent = user.name
             row.querySelector('.user a').href = `https://github.com/${user.login}`
-            row.querySelector('.user span').textContent = user.login
+            row.querySelector('.user span').textContent = `/${user.login}`
             row.querySelector('.repositories').textContent= user.public_repos
             row.querySelector('.followers').textContent= user.followers
 
@@ -116,7 +116,7 @@ export class FavoritesView extends Favorites {
             6
         </td>
         <td>
-            <button class="remove">&times;</button>
+            <button class="remove">Remover</button>
         </td>
         `
         return tr
